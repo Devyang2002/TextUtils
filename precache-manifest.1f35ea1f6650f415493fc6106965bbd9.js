5368969f8230147959a04a2553fd79356e3f42d0
Devyang saini <89840695+Devyang2002@users.noreply.github.com>
2023-05-25 20:09:09 +0530

    Updates

---
+++ precache-manifest.1f35ea1f6650f415493fc6106965bbd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6383da5307c1e4ee3bf2",
+    "url": "/TextUtils-React/static/css/main.2bd5beab.chunk.css"
+  },
+  {
+    "revision": "6383da5307c1e4ee3bf2",
+    "url": "/TextUtils-React/static/js/main.6383da53.chunk.js"
+  },
+  {
+    "revision": "8c8ed3e6ce86a909c145",
+    "url": "/TextUtils-React/static/js/1.8c8ed3e6.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "e04156fa5bf18b1e9eb9",
+    "url": "/TextUtils-React/static/js/runtime~main.e04156fa.js"
+  },
+  {
+    "revision": "69dbac736ccff8b1a092e61ceb7e8633",
+    "url": "/TextUtils-React/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.eb7b994821178938baa25a95fa96c180.js"
+  "/TextUtils-React/precache-manifest.1f35ea1f6650f415493fc6106965bbd9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
+workbox.routing.registerNavigationRoute("/TextUtils-React/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
